69b079c3f149875bf9b53d570f97482dfea9175a
Justin Cox <Justinkcox@ymail.com>
2022-10-17 11:17:46 -0500

    Change userModel `role` to enum with default value

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,6 +20,8 @@ const userSchema = mongoose.Schema(
     },
     role: {
       type: String,
+      enum: ["user", "admin"],
+      default: "user",
       required: [true, "Role is required"],
     },
   },
