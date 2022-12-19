67424d4f243564b43b37b20cecadb134c041961f
Justin Cox <Justinkcox@ymail.com>
2022-12-19 13:16:07 -0600

    Reverting back to af1c460

--- frontend/src/pages/Calculator.jsx
+++ frontend/src/pages/Calculator.jsx
@@ -1,9 +1,9 @@
 function Calculator() {
-    return (
-        <section>
-            <h1>Calculator-</h1>
-        </section>
-    )
+  return (
+    <section>
+      <h1>Calculator</h1>
+    </section>
+  );
 }
 
-export default Calculator
+export default Calculator;
