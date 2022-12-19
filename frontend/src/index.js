d4e5facc8aa7729f773d95e36ab292795e272884
Justin Cox <Justinkcox@ymail.com>
2022-12-19 13:47:04 -0600

    Comment out ReactQueryDevTools in frontend/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 // import { Provider } from "react-redux";
 // import { store } from "./app/store";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./index.scss";
-import "primereact/resources/themes/mdc-light-indigo/theme.css"; //theme
-import "primereact/resources/primereact.min.css"; //core css
-import "primeicons/primeicons.css"; //icons
-import "../node_modules/primeflex/primeflex.css";
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import './index.scss'
+import 'primereact/resources/themes/mdc-light-indigo/theme.css' //theme
+import 'primereact/resources/primereact.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+import '../node_modules/primeflex/primeflex.css'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
 
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
-);
+    <QueryClientProvider client={queryClient}>
+        <App />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
