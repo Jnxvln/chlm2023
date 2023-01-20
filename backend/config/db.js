6006237d3ca62343ebe6919258c30defb8a63e76
Justin Cox <Justinkcox@ymail.com>
2023-01-20 08:18:33 -0600

    Log mongo URIs for test purposes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
+    console.log(`MONGODB_URI_21: ${process.env.MONGODB_URI_21}`)
+    console.log(`MONGODB_URI_23: ${process.env.MONGODB_URI_23}`)
+    console.log(`MONGODB_URI_DEV: ${process.env.MONGODB_URI_DEV}`)
+
     try {
         let conn
 
